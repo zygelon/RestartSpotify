cf321263774f4ca11bf39998b7e3b7fba8f34c30
mbondar <mbondar@stalker2.com>
2022-11-06 06:09:17 +0100

    WIP

--- RestartSpotify.py
+++ RestartSpotify.py
@@ -1,6 +1,14 @@
 import os
+import distutils.spawn
 
 if __name__ == '__main__':
     os.system(r"taskkill /f /im Spotify.exe")
+    if not distutils.spawn.find_executable("Spotify.exe"):
+        SpotifyDirectory = os.path.expanduser("~") + r'\AppData\Roaming\Spotify'
+        AddPathCommand = 'SET PATH=%PATH%;' + SpotifyDirectory
+        os.system(AddPathCommand)
+
+    #assert distutils.spawn.find_executable("Spotify.exe")
     #Plz add Spotify.exe into Register or PATH https://docs.microsoft.com/en-us/windows/win32/shell/app-registration
+
     os.system("start Spotify.exe")
